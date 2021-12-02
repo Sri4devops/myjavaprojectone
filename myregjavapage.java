63c0e49d30f295c2a606c3b3d1927ac9fdaff467
srinu <amruthamsri@gmail.com>
2021-12-02 18:56:22 +0530

    100% work completed

--- myregjavapage.java
+++ myregjavapage.java
@@ -1,2 +1,11 @@
-this is reg page
-do manager work comleted
+<<<<<<< Updated upstream
+this is java reg page
+do manager work completed
+=======
+this is java reg page
+1st day lines of code
+2nd day lines of code
+3rd day lines of code
+4th day lines of code
+5th day lines of code
+
